d10d7518d9590f5d0d7b3eb1fd05c26d0a71f480
Rain-1214 <13131459087@163.com>
2018-04-27 18:30:35 +0800

    newbegin

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,22 @@
 import * as React from 'react';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import todoApp from './store/reducers';
-import AddTodo from './containers/AddTodo';
-import VisibleTodoList, { ShowType } from './containers/VisibleTodoList';
-import Footer from './component/Redux/Footer/Footer';
-import logger from './store/middleware/Logger';
-// import thunk from 'redux-thunk';
-import createSagaMiddleware from 'redux-saga';
-import { BrowserRouter as Router, Route, Redirect, Link } from 'react-router-dom';
-import UserContainers from './containers/user';
-import Count from './containers/Count';
-import UserSaga from './store/saga/UserSaga';
+// import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
 
-const sagaMiddleware = createSagaMiddleware();
-
-const stroe = createStore(
-  todoApp,
-  applyMiddleware(
-    logger,
-    // thunk,
-    sagaMiddleware
+const S = (props: any) => {
+  return (
+    <div>
+      123
+    </div>
   )
-);
-
-sagaMiddleware.run(UserSaga);
+}
 
 class App extends React.Component {
-  render() {
+  public render(): any {
     return (
-      <Provider store={stroe}>
-        <Router>
-          <div>
-            <AddTodo />
-            <hr/>
-            <Route exact={true} path="/" render={(props) => (
-               <Redirect to={{
-                 pathname: `/${ShowType.SHOW_ALL}`,
-                 state: { from: props.location }
-               }} />
-            )}/>
-            <Route path="/:filter" render={(props) => {
-              return (
-                <div>
-                  <Footer {...props} />
-                  <VisibleTodoList {...props} />
-                </div>
-              );
-            }} />
-            <hr/>
-            <Link to="/user">user</Link>
-            <br/>
-            <Link to="/count">count</Link>
-            <hr/>
-            <Route path="/user" component={UserContainers} />
-            <Route path="/count" render={() => (
-              <div>
-                <Count id="a" />
-                <Count id="b" />  
-              </div>
-            )} />
-          </div>
-        </Router>
-      </Provider>
+      <Router>
+        <Route path="/" exact={true} component={S} />
+      </Router>
     );
   }
 }
--- src/store/saga/saga.ts
+++
@@ -1,7 +0,0 @@
-
-export const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
-
-export function* hellowSaga () {
-  yield delay(1000);
-  console.log('hellow saga');
-}
